617b257a018538cc0033d5788e78e7f30805cd6c
hamzaselteq <hamza@selteq.net>
2022-05-31 14:50:14 +0500

    First commit

---
+++ src/components/DashboardLayout.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { DashboardNavbar } from './DashboardNavbar';
+import DashboardSidebar from './DashboardSidebar';
+import Dashboard from '../pages/index';
+
+
+const DashboardLayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  paddingTop: 64,
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: 280
+  }
+}));
+
+const  DashboardLayout = (props) => {
+  const { children } = props;
+  console.log("children is :" + children);
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <>
+      <DashboardLayoutRoot>
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+            flexDirection: 'column',
+            width: '100%'
+          }}
+        >
+          {/* {children} */}
+          {/* dashboard index start  */}
+         <Dashboard />
+          {/* dashboard index end  */}
+
+        </Box>
+      </DashboardLayoutRoot>
+      <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
+      <DashboardSidebar
+        onClose={() => setSidebarOpen(false)}
+        open={isSidebarOpen}
+      />
+    </>
+  );
+};
+
+export default DashboardLayout;
